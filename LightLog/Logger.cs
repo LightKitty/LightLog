11ec6ed30069426a05472acda2bc59d74bcf4d92
yangguanghui <yangguanghui@beisen.com>
2020-09-08 19:18:46 +0800

    日志增加级别

--- LightLog/Logger.cs
+++ LightLog/Logger.cs
@@ -20,31 +20,54 @@ namespace LightLog
         private string logFolderPath = "log\\";
 
         /// <summary>
-        /// 日志类型
+        /// 日志级别
         /// </summary>
-        private enum LogType { Info, Error };
+        private enum LogLevel { Debug, Info, Warn, Error, Fatal };
 
         public Logger()
         {
-            if (!Directory.Exists(logFolderPath)) Directory.CreateDirectory(logFolderPath);
+            if (!Directory.Exists(logFolderPath)) Directory.CreateDirectory(logFolderPath); //判断并创建日志文件夹
         }
 
         public Logger(string path)
         {
             logFolderPath = (path.EndsWith("\\") || path.EndsWith("/")) ? path : path + "\\";
-            if (!Directory.Exists(logFolderPath)) Directory.CreateDirectory(logFolderPath);
+            if (!Directory.Exists(logFolderPath)) Directory.CreateDirectory(logFolderPath); //判断并创建日志文件夹
+        }
+
+        /// <summary>
+        /// 写调试日志
+        /// </summary>
+        /// <param name="msg"></param>
+        public void Debug(string msg, Exception ex = null)
+        {
+            Task.Run(() =>
+            {
+                WriteLog(LogLevel.Debug, msg, ex);
+            });
         }
 
         /// <summary>
         /// 写信息日志
         /// </summary>
         /// <param name="msg"></param>
-        public void Info(string msg)
+        public void Info(string msg, Exception ex = null)
         {
             Task.Run(() =>
             {
-                msg = GetMsgHead(LogType.Info) + msg + Environment.NewLine;
-                WriteLog(GetLogFilePath(), msg);
+                WriteLog(LogLevel.Info, msg, ex);
+            });
+        }
+
+        /// <summary>
+        /// 写警告日志
+        /// </summary>
+        /// <param name="msg"></param>
+        public void Warn(string msg, Exception ex = null)
+        {
+            Task.Run(() =>
+            {
+                WriteLog(LogLevel.Warn, msg, ex);
             });
         }
 
@@ -53,23 +76,47 @@ namespace LightLog
         /// </summary>
         /// <param name="msg"></param>
         /// <param name="ex"></param>
-        public void Error(string msg, Exception ex)
+        public void Error(string msg, Exception ex = null)
         {
             Task.Run(() =>
             {
-                msg = GetMsgHead(LogType.Error) + msg + Environment.NewLine + ex.ToString() +Environment.NewLine; //ex.ToString()内容最详细
-                WriteLog(GetLogFilePath(), msg);
+                WriteLog(LogLevel.Error, msg, ex);
             });
+        }
+
+        /// <summary>
+        /// 写致命日志
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="ex"></param>
+        public void Fatal(string msg, Exception ex = null)
+        {
+            Task.Run(() =>
+            {
+                WriteLog(LogLevel.Fatal, msg, ex);
+            });
+        }
+
+        /// <summary>
+        /// 获取日志内容
+        /// </summary>
+        /// <param name="logLevel"></param>
+        /// <param name="msg"></param>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        private string GetLogContent(LogLevel logLevel, string msg, Exception ex)
+        {
+            return GetLogHead(LogLevel.Error) + msg + Environment.NewLine + (ex == null ? string.Empty : (ex.ToString() + Environment.NewLine)); //ex.ToString()内容最详细
         }
 
         /// <summary>
         /// 日志头部信息
         /// </summary>
-        /// <param name="logType"></param>
+        /// <param name="logLevel"></param>
         /// <returns></returns>
-        private string GetMsgHead(LogType logType)
+        private string GetLogHead(LogLevel logLevel)
         {
-            return $"[{DateTime.Now.ToString("HH:mm:ss")} {logType.ToString()}]";
+            return $"[{DateTime.Now.ToString("HH:mm:ss")} {logLevel.ToString()}]";
         }
 
         /// <summary>
@@ -79,6 +126,17 @@ namespace LightLog
         private string GetLogFilePath()
         {
             return logFolderPath + DateTime.Now.ToString("yyyyMMdd") + ".log";
+        }
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="logLevel"></param>
+        /// <param name="msg"></param>
+        /// <param name="ex"></param>
+        private void WriteLog(LogLevel logLevel, string msg, Exception ex)
+        {
+            WriteLog(GetLogFilePath(), GetLogContent(LogLevel.Error, msg, ex)); //格式化日志、写日志
         }
 
         /// <summary>
